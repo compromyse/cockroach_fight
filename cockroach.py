ed5c28b2df28d123dc67304dc57ddd4092fa72ee
Raghuram Subramani <raghus2247@gmail.com>
2023-07-30 14:18:24 +0530

    clean up cockroach

---
+++ cockroach.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+
+
+class Cockroach:
+    def __init__(self):
+        self.x = random.randint(80, 800)  # Random x position within the screen width
+        self.y = 60  # Starting y position at the top
+        self.speed_y = random.randint(1, 5)  # Random downward speed
+
+        # Load cockroach image
+        self.cockroach_image = pygame.image.load('res/cockroach.png')
+
+    def move_down(self):
+        self.y += self.speed_y
+
+    def draw(self, screen):
+        screen.blit(self.cockroach_image, (self.x, self.y))
