c9062fa7e82e39a6019686814248038b5c0bdaf3
Raghuram Subramani <raghus2247@gmail.com>
2023-07-30 16:35:05 +0530

    use rtsp

--- game.py
+++ game.py
@@ -13,13 +13,13 @@ from asteroid import Asteroid
 pygame.init()
 
 # Define screen dimensions
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
 
 class MyGame:
     def __init__(self) -> None:
         # Open the webcam (0 is usually the default)
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture('rtsp://10.20.89.139:8080/h264_aac.sdp')
 
         # Create a list to hold cockroaches
         self.cockroaches = []
@@ -139,6 +139,7 @@ class MyGame:
 
             # Rotate the frame counter-clockwise by 90 degrees
             frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            frame = cv2.flip(frame, 0)
 
             # Convert frame from RGB to HSV, and define frame used for detection
             det_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
