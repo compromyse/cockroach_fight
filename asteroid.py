28d7cee3ed843e4cb183ebe53f7a508d4df6d3de
Raghuram Subramani <raghus2247@gmail.com>
2023-07-30 14:30:26 +0530

    add asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,17 +1,17 @@
 import pygame
 import random
 
-# Load asteroid image
-asteroid_image = pygame.image.load('res/asteroid.png')
-
 class Asteroid:
     def __init__(self):
         self.x = random.randint(80, 800)  # Random x position within the screen width
         self.y = 60  # Starting y position at the top
         self.speed_y = random.randint(1, 3)  # Random downward speed
 
+        # Load asteroid image
+        self.asteroid_image = pygame.image.load('res/asteroid.png')
+
     def move_down(self):
         self.y += self.speed_y
 
     def draw(self, screen):
-        screen.blit(asteroid_image, (self.x, self.y))
+        screen.blit(self.asteroid_image, (self.x, self.y))
--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
+
 import pygame
 import random
 import threading
+
 from cockroach import Cockroach
+from asteroid import Asteroid
 
 # Initialize Pygame
 pygame.init()
@@ -33,6 +36,7 @@ class MyGame:
     def run_cockroach_game(self):
         # Create a list to hold cockroaches
         cockroaches = []
+        asteroids = []
 
         while not self.stop_event.is_set():
             for event in pygame.event.get():
@@ -49,6 +53,16 @@ class MyGame:
                 cockroach.move_down()
                 if cockroach.y >= SCREEN_HEIGHT:
                     cockroaches.remove(cockroach)
+
+            # Spawn a new asteroid randomly
+            if len(asteroids) < 5 and random.random() < 0.02:
+                asteroids.append(Asteroid())
+
+            # Move asteroid downwards and remove off-screen ones
+            for asteroid in asteroids:
+                asteroid.move_down()
+                if asteroid.y >= SCREEN_HEIGHT:
+                    asteroids.remove(asteroid)
 
             # Read frame from the webcam
             ret, frame = self.cap.read()
@@ -92,6 +106,10 @@ class MyGame:
             # Draw the cockroach panel on top of the camera feed
             for cockroach in cockroaches:
                 cockroach.draw(self.screen)
+
+            # Draw the cockroach panel on top of the camera feed
+            for asteroid in asteroids:
+                asteroid.draw(self.screen)
 
             # Update the display
             pygame.display.flip()
